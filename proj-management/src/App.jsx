2f5b20948c7192bbc3f03b707ee3c970342dde4f
Desmond <desmondmpofu@gmail.com>
2024-04-25 20:24:58 +0200

    delete project

--- proj-management/src/App.jsx
+++ proj-management/src/App.jsx
@@ -27,6 +27,17 @@ function handleSelectProject(id){
      })
     }
 
+function handleDeleteProject(){
+    setProjectsState(prevState=>{
+        return {
+            ...prevState,
+            selectedProjectId:undefined,
+            projects: prevState.projects.filter((project)=>project.id!==prevState.selectedProjectId)
+
+        }
+     })
+}
+
 
 function handleCancelProject(){
     setProjectsState(prevState=>{
@@ -56,11 +67,11 @@ function handleAddProject(projectData){
 const selectedProject=projectsState.projects.find(project=>project.id===projectsState.selectedProjectId)
 
  console.log('### ', projectsState)
-    let content = <SelectedProject project={selectedProject}/>
+    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
     if (projectsState.selectedProjectId===null){
         content=<NewProject onAdd={handleAddProject} onCancel={handleCancelProject}/>
     }else if (projectsState.selectedProjectId===undefined){
-        content =<NoProjectSelected onStartAddProject = {handleStartAddProject}/>
+        content = <NoProjectSelected onStartAddProject = {handleStartAddProject}/>
     }
   return (
     <main className="h-screen my-8 flex gap-8">
