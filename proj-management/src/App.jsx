b64bddad9fa4534077819419355b1de520d25973
Desmond <desmondmpofu@gmail.com>
2024-04-28 12:20:22 +0200

    delete tasks

--- proj-management/src/App.jsx
+++ proj-management/src/App.jsx
@@ -27,7 +27,14 @@ function handleAddTask(text){
 
 }
 
-function handleDeleteTask(){}
+function handleDeleteTask(id){
+    setProjectsState((prevState)=>{
+        return{
+            ...prevState,
+            tasks: prevState.tasks.filter((task)=>task.id !==id)
+        }
+    })
+}
 
 function handleStartAddProject(){
      setProjectsState(prevState=>{
